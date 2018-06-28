a154fc5a0875562be171864bfae87d299ad0f49b
Diarmuid Morgan <diarmuidmorgan@hotmail.com>
2018-06-29 00:29:10 +0100

    ignore ipynb checkpoints

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='dbanalysis',
-        packages=['dbanalysis','dbanalysis.models','dbanalysis.network','dbanalysis.models','dbanalysis.classes','dbanalysis.app','dbanalysis.plots'],
+        packages=['dbanalysis','dbanalysis.scripts','dbanalysis.models','dbanalysis.network','dbanalysis.models','dbanalysis.classes','dbanalysis.app','dbanalysis.plots'],
         include_package_data=True
 )
