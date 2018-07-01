d42348fb81a1efac31b1ac58df792665aae1e925
Diarmuid Morgan <diarmuidmorgan@hotmail.com>
2018-07-01 20:04:36 +0100

    mapped stops to shapes. Created distance calculator between linked stops

--- dbanalysis/stop_tools.py
+++ dbanalysis/stop_tools.py
@@ -3,7 +3,7 @@
 Set of functions for retrieving and analyzing stop-stop links. 
 
 """
-
+import haversine
 
 def get_stop_link(stopA,stopB, src='file',merge_weather=False):
     
@@ -43,4 +43,53 @@ def get_stop_link(stopA,stopB, src='file',merge_weather=False):
     else:
         return df
     
+class stop_getter():
 
+    def __init__(self):
+        import pickle
+        import json
+        import haversine
+        base_dir = '/home/student/dbanalysis/dbanalysis/resources/'
+        with open(base_dir+'trimmed_stops_shapes_map.pickle','rb') as handle:
+            self.stops_map = pickle.load(handle)
+
+        self.stops_dict = json.loads(open(base_dir+'stops_trimmed.json','r').read())
+
+    def get_stop_coords(self,stop):
+
+        if stop in self.stops_dict:
+           return {'lat':self.stops_dict[stop]['lat'],
+                    'lon':self.stops_dict[stop]['lon']}
+
+        else:
+            return None
+
+    def get_stop_links(self, stop):
+        if stop in self.stops_map:
+            return [stop for stop in self.stops_map[stop]]
+        else:
+            return None
+
+    def get_stop_distance(self,stop,link):
+        
+        if stop in self.stops_map and link in self.stops_map[stop]:
+
+            coords = [self.get_stop_coords(stop)] + self.stops_map[stop][link]\
+                            + [self.get_stop_coords(link)]
+            total_distance = 0
+            for i in range(0, len(coords)-1):
+                lat1 = coords[i]['lat']
+                lon1=coords[i]['lon']
+                lat2=coords[i+1]['lat']
+                lon2=coords[i+1]['lon']
+                total_distance += haversine.haversine((lat1,lon1),(lat2,lon2))
+            return total_distance
+        else:
+            return None
+            
+
+if __name__ == '__main__':
+    s = stop_getter()
+    print(s.stops_dict.keys())
+    a=s.get_stop_links('7574')[0]
+    print(s.get_stop_distance('7574',a))
